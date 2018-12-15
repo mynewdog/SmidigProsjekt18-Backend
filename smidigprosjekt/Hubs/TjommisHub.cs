ba0a6f10a26cf0a69a37d9c6a9d0507e6563d047
Erik Alvarez Tyldum <erik@garnesdata.no>
2018-12-15 20:29:26 +0100

    First commit

---
+++ smidigprosjekt/Hubs/TjommisHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace smidigprosjekt.Hubs
+{
+  [EnableCors("AllowAllHeaders")]
+  public class TjommisHub : Hub
+  {
+
+    public async Task SendMessage(string user, string message)
+    {
+      object[] args = { user, message };
+      await Clients.All.SendAsync("messageBroadcastEvent", user, message);
+    }
+    public object Authenticate(string username, string password) { 
+      if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password)) { 
+        return new { authenticated=true } ;
+      }
+      else
+      {
+        return new { authenticated = false };
+      }
+    }
+  }
+}
