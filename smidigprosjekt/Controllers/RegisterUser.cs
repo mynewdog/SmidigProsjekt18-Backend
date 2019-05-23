e54ba432e027e74104e1db64b8d87b74b4ae5f13
Sander Sjøthun <sander.sjothun@gmail.com>
2019-05-23 20:10:39 +0200

    Added Ok/BadRequest to regUser

--- smidigprosjekt/Controllers/RegisterUser.cs
+++ smidigprosjekt/Controllers/RegisterUser.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using smidigprosjekt.Logic.Services;
+using smidigprosjekt.Models;
 
 namespace smidigprosjekt.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("[controller]")]
   [ApiController]
   public class RegisterUser : ControllerBase
   {
-    [HttpPost]
-    public void Post([FromBody] NewUserClass newUser)
-    {
+    private IUserService _userService;
 
+    public RegisterUser(IUserService UserService) {
+      _userService = UserService;
+    }
+    //Todo: legg inn Password field
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] NewUserClass newUser)
+    {
+      var user = new User() {
+        Username = newUser.Name,
+      };
+            user.storePassword(newUser.Password);
+      //Ny klasse for hver post
+      if (await _userService.RegisterUser(user))
+      {
+        return Ok(user);
+      }
+      else {
+        return BadRequest();
+      }
     }
   }
 }
---
+++ smidigprosjekt/NewUserClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using smidigprosjekt.Controllers;
+
+namespace smidigprosjekt
+{
+    public class NewUserClass
+    {
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Institutt { get; set; }
+        public String Studie { get; set; }
+    }
+}
