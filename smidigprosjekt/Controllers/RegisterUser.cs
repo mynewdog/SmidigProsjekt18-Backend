25a9bbc4c36bf766aef0eabc4a91f64519df08f1
Sander Sjøthun <sander.sjothun@gmail.com>
2019-05-23 18:38:03 +0200

    Added new controller

---
+++ smidigprosjekt/Controllers/RegisterUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace smidigprosjekt.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RegisterUser : ControllerBase
+  {
+    [HttpPost]
+    public void Post([FromBody] NewUserClass newUser)
+    {
+
+    }
+  }
+}
