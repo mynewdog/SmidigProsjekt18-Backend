4924714646fd2b15a94e7d3eec6437ba375ede0f
Sander Sjøthun <sander.sjothun@gmail.com>
2019-05-24 20:36:59 +0200

    Halfway done with matching algo

--- smidigprosjekt/Logic/Services/LobbyService.cs
+++ smidigprosjekt/Logic/Services/LobbyService.cs
@@ -1,9 +1,11 @@
 ﻿using smidigprosjekt.Logic.Database;
 using smidigprosjekt.Models;
+using smidigprosjekt.Hubs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace smidigprosjekt.Logic.Services
 {
@@ -12,6 +14,7 @@ namespace smidigprosjekt.Logic.Services
         IEnumerable<Lobby> All();
         void Add(Lobby lobby);
         void Delete(Lobby lobby);
+        void SendRoom(Lobby lobby);
         int Count();
         Lobby GetTemporary();
     }
@@ -22,40 +25,63 @@ namespace smidigprosjekt.Logic.Services
     /// </summary>
     public class LobbyService : ILobbyService
     {
+        private AppConfiguration _appConfig;
+        private IUserService _userService { get; set; }
+        private IHubContext<TjommisHub> _hub { get; set; }
         //List of the all the Lobbies
-        
         public List<Lobby> Lobbies { get; set; }
 
         public LobbyService()
         {
             Lobbies = new List<Lobby>();
         }
+        // Active rooms save to Firebase
         public void Add(Lobby lobby)
         {
             Lobbies.Add(lobby);
             Task.Run(() => FirebaseDbConnection.saveRooms(Lobbies));
         }
+        /// <summary>
+        /// TODO:
+        /// </summary>
+        /// <param name="lobby"></param>
+        public void SendRoom(Lobby lobby)// add parameter of composistion tags
+        {
+            if(Count() > 1) // too few active rooms
+            {
+                if (lobby.MaxUsers == lobby.Members.Count) // Room reach max size
+                {
+                    lobby.Joinable = false; // no longer in the pool of temp rooms
+                    _hub.Clients.Group(lobby.LobbyName).SendAsync("JoinRoom", lobby);
+                    Add(lobby); // Firebase
+                }
+                else
+                {
+                    // Try to merge with most equal room composition.
+                    // then various not filled rooms
+                    // recursive asking after merge to see if rooms are max.
 
+                }
+            }
+            // Only 1 room active, not wait for users, no matching
+            else
+            {
+                lobby.Joinable = false; // no longer in the pool of temp rooms
+                _hub.Clients.Group(lobby.LobbyName).SendAsync("JoinRoom", lobby);
+                Add(lobby); // Firebase
+            }            
+        }
+
+        /// <summary>
+        /// TODO: Will match compostion and move users
+        /// </summary>
+        /// <param name="lobby1"></param>
+        /// <param name="lobby2"></param>
         public void Merge(Lobby lobby1, Lobby lobby2)
         {
             //lobb1 ++ lobby2
             // -> users lobby2) -||-.users
             // delete lobby1
-        }
-
-        public void GetTempRoom(UserService user)
-        {
-            /*
-             for each(tempRoom)
-             {
-                if(tempRoom.match(User) => 1{
-                    let tempRoom
-                }
-                else
-                    let new tempRoom(User);
-             }
-             
-             */
         }
 
         public IEnumerable<Lobby> All()
@@ -75,7 +101,25 @@ namespace smidigprosjekt.Logic.Services
 
         public Lobby GetTemporary()
         {
-            throw new NotImplementedException();
+            var rnd = new Random();
+            var availableRooms = Lobbies.Where(e => e.Joinable).OrderByDescending(e => e.Members.Count);
+            // Creates a new room
+            if (availableRooms.Count() == 0)
+            {
+                Lobby room = new Lobby()
+                {
+                    Id = rnd.Next(),
+                    Members = new System.Collections.Concurrent.ConcurrentDictionary<int, User>(),
+                    Messages = new List<Message>(),
+                    MaxUsers = _appConfig.MaximumPerLobby
+                };
+                return room;
+            }
+            // Returns the most populated room
+            else
+            {
+                return availableRooms.First();
+            }
         }
     }
 }
--- smidigprosjekt/Models/Lobby.cs
+++ smidigprosjekt/Models/Lobby.cs
@@ -8,9 +8,14 @@ namespace smidigprosjekt.Models
 {
     public class Lobby
     {
+        public bool Joinable { get; set; } // to add/remove it-> pool of lobbies
         public int Id { get; set; }
         public string LobbyName { get; set; }
+        public string Institutt { get; set; } // skole
+        public string Studie { get; set; } // fag
+        //public string Interesser { get; set; } // ikke brukt atm.
         public ConcurrentDictionary<int, User> Members { get; set; }
         public List<Message> Messages { get; set; }
+        public int MaxUsers { get; set; } // will change depening on 1-1 or (+) button
     }
 }
